d2f44cda7f162a91b936e2e74a2d65a1b5a5648d
Christophe Chapuis <chris.chapuis@gmail.com>
2022-04-18 08:59:28 +0000

    Handle "noWindow" apps
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>
    
    Upstream-Status: Pending

--- src/base/AppDescription.h
+++ src/base/AppDescription.h
@@ -299,6 +299,13 @@ public:
         return requestLuneOSStyle;
     }
 
+    bool hasNoWindow() const
+    {
+        bool requestNoWindow = false;
+        JValueUtil::getValue(m_appinfo, "noWindow", requestNoWindow);
+        return requestNoWindow;
+    }
+
 private:
     static const vector<string> PROPS_PROHIBITED;
     static const vector<string> PROPS_IMAGES;
